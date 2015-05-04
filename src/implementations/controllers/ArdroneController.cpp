81505a423aded341bd14f5f04591f5b509bb703b
Bardo91 <19Bardo91@gmail.com>
2015-05-04 22:23:22 +0200

    Rearranged files and imported existing implementations of controllers and sensors from euroarms project

---
+++ src/implementations/controllers/ArdroneController.cpp
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	EUROARMS:	Bar Task
+//		Author:	Pablo Ramon Soria
+//		Date:	2015-APR-10
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+
+
+#include "ArdroneController.h"
+
+void ArdroneController::parseAction(const Message& _message){
+	switch (_message.type())
+	{
+	case MessageType::eLanding:
+		mDrone.land();
+		break;
+	case MessageType::eTakeOff:
+		mDrone.takeOff();
+		break;
+	case MessageType::eHovering:
+		mDrone.hovering();
+		break;
+	case MessageType::eMove:{
+		std::string payload = _message.payload();
+		float pitch = float(atof(payload.substr(0, payload.find(',')).c_str()));
+		float roll = float(atof(payload.substr(payload.find(','), payload.size()).c_str()));
+		mDrone.translate(pitch, roll);
+		break;
+	}default:
+		//std::cout << "Controller: Message of type: " << _message.type() << " is not supported in this controller." << std::endl;
+		break;
+	}
+}
