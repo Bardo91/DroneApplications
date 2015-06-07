b7850b8a4e32e151156edb6f2e064f0e825695b8
Bardo91 <19Bardo91@gmail.com>
2015-06-07 10:46:17 +0200

    Created general ImuSensor class to be extended.

---
+++ src/implementations/sensors/ImuSensor.h
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	EUROARMS:	Bar Task
+//		Author:	Pablo Ramon Soria
+//		Date:	2015-JUN-07
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+
+#ifndef _DRONE_APPLICATIONS_IMPLEMENTATIONS_SENSORS_IMU_SENSOR_H_
+#define _DRONE_APPLICATIONS_IMPLEMENTATIONS_SENSORS_IMU_SENSOR_H_
+
+#include "../../SensorManager.h"
+
+#include <array>
+#include <mutex>
+
+class ImuData{
+	std::array<double, 3>	mEulerAngles;
+	std::array<double, 3>	mPos;
+	double					mAltitude;
+};
+
+typedef SensorTrait<SensorType::eIMU, ImuData> ImuTrait;
+
+
+class ImuSensor : public Sensor<ImuTrait>{
+public:		// Public Interface
+	ImuSensor();
+	ImuSensor(ImuSensor&) {};
+
+	ImuData get(){
+		std::lock_guard<std::mutex> lock(mSecureMutex);
+		return mLastData;
+
+	}
+
+private:	//	 Members
+	std::mutex	mSecureMutex;
+	ImuData		mLastData;
+};
+
+#endif	//	_DRONE_APPLICATIONS_IMPLEMENTATIONS_SENSORS_IMU_SENSOR_H_
