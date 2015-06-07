d33d222c9956e64423ecedae2265a276b57e5473
Bardo91 <19Bardo91@gmail.com>
2015-06-07 11:04:02 +0200

    Documented DroneApplication class with doxygen

--- src/DroneApplication.h
+++ src/DroneApplication.h
@@ -17,13 +17,33 @@
 #include <queue>
 #include <mutex>
 
+/**	Main class that holds Drone's application classes.	777 review pipeline.
+*
+*/
 class DroneApplication{
 public:
+	/** \brief add new command to controller's queue.
+	*	@param	_message: message to be added to the queue.
+	*/
 	void addCommand		(const Message& _message);
+	
+	/** \brief @deprecated
+	*/
 	void step			();
 
+	/** \brief register new sensor in application.
+	*	@param _sensor:	instance of sensor to be added.
+	*/
 	void registerSensor	(SensorBase& _sensor);
+	
+	/** \brief set new controller to drone's application. Only one controller can be assigned at once.
+	*	@param _controller: instance of controller to be added.
+	*/
 	void setController	(Controller& _controller);
+	
+	/** \brief attach new task to drone's applcation. Currently only asynchronous tasks can be added.
+	*	@param _task: task to be added.
+	*/
 	void attachProcess	(TaskBase& _task);
 private:
 	std::priority_queue<Message, std::vector<Message>, Message>		mQueuedMessages;
