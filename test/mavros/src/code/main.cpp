8447083756e6d9696744eff3614ab2fd22e03be6
Bardo91 <19Bardo91@gmail.com>
2015-06-10 06:20:18 +0930

    Added mavros test case prj config

---
+++ test/mavros/src/code/main.cpp
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	EUROARMS:	Bar Task
+//		Author:	Pablo Ramon Soria
+//		Date:	2015-02-05
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This is the main of the program.This program is an implementation to solve the trouble of catching bars
+// in order to build afterwards structures.
+
+#include <DroneApplication.h>
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(int _argc, char** _argv){
+	_argc; _argv;
+	DroneApplication mainApp;
+	std::cout << "LASLALS" << std::endl;
+	for (;;){
+		mainApp.step();
+	}
+
+	//system("PAUSE");
+
+}
