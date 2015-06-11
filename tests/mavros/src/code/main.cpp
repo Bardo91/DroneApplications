8e1caf92d9321205024d699fed9fabd66c242242
Bardo91 <19Bardo91@gmail.com>
2015-06-11 15:35:12 +0200

    Check ROS include in test mavros in order to generate messages of WARNINGS instead of compile errors.

--- tests/mavros/src/code/main.cpp
+++ tests/mavros/src/code/main.cpp
@@ -17,23 +17,27 @@
 using namespace std;
 
 int main(int _argc, char** _argv){
-	_argc; _argv;
-	ros::init(_argc, _argv,"MavrosSensorTest");
+	#if defined(_HAS_ROS_LIBRARIES_)
+		_argc; _argv;
+		ros::init(_argc, _argv,"MavrosSensorTest");
 	
-	DroneApplication mainApp;
+		DroneApplication mainApp;
 	
-	MavrosSensor imuSensor;
+		MavrosSensor imuSensor;
 	
-	ros::Rate loop_rate(10);
+		ros::Rate loop_rate(10);
 	
-	while(ros::ok()){
-		mainApp.step();
-		ImuData data = imuSensor.get();
-		std::cout << data.mEulerAngles[0] << ", " << data.mEulerAngles[1] << ", " << data.mEulerAngles[2] << std::endl;
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
-
+		while(ros::ok()){
+			mainApp.step();
+			ImuData data = imuSensor.get();
+			std::cout << data.mEulerAngles[0] << ", " << data.mEulerAngles[1] << ", " << data.mEulerAngles[2] << std::endl;
+			ros::spinOnce();
+			loop_rate.sleep();
+		}
+	#else
+		std::cout << "[WARNING] Application compiled without ROS libraries" << std::endl;
+		return -1;
+	#endif
 	//system("PAUSE");
 
 }
