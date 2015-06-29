9571cb991dbbefff63c98d20490d5ad92a0371c2
Bardo91 <19Bardo91@gmail.com>
2015-06-29 02:48:59 +0200

    Changing task interface for proper managing of life cycle.

--- src/code/Task.h
+++ src/code/Task.h
@@ -32,10 +32,16 @@ public:
 	template<typename SensorClassType_>
 	SensorClassType_* getSensor();
 
+	virtual void start()	= 0;
+	virtual void stop()		= 0;
+
 protected:
 	friend class DroneApplication;
 	TaskBase();
 	void	attach(std::priority_queue<Message, std::vector<Message>, Message> *_queue, SensorManager *_sensorMgr);
+
+	std::thread		*mThread;
+	bool			mIsRunning = false;
 
 private:
 	std::priority_queue<Message, std::vector<Message>, Message>		*mDroneQueue;
@@ -45,15 +51,18 @@ private:
 //---------------------------------------------------------------------------------------------------------------------
 /// Task interface that implements the thread of the asyncronous tasks. 777 Add example.
 template<typename SonClass_>
-class Task: public TaskBase{
+class Task: protected TaskBase{
 public:
 	/// \brief Task action. Loop is not implemented, task cycle is defined inside this method.
+	virtual void init() = 0;
 	virtual void run() = 0;
 
 	/// \brief Start task.
 	void start();
+	void stop();
 private:
-	std::thread		*mThread;
+	void callback();
+
 };
 
 #include "Task.inl"
--- src/code/Task.inl
+++ src/code/Task.inl
@@ -16,7 +16,26 @@ SensorClassType_* TaskBase::getSensor(){
 template<typename SonClass_>
 void Task<SonClass_>::start(){
 	mThread = new std::thread(&SonClass_::run, reinterpret_cast<SonClass_*>(this));
+	mIsRunning = true;
 	write2Log("Starting Task");
 }
 
 //---------------------------------------------------------------------------------------------------------------------
+template<typename SonClass_>
+void Task<SonClass_>::stop(){
+	mIsRunning = stop;
+	if(mThread->joinable())
+		mThread->join();
+
+	write2Log("Task Stoped");
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+template<typename SonClass_>
+void Task<SonClass_>::callback(){
+	init();
+
+	while (mIsRunning) {
+		run();
+	}
+}
