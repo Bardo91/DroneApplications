aceb6672111b9c01bb556338b29aab85ee04a910
Bardo91 <19Bardo91@gmail.com>
2015-06-29 02:41:00 +0200

    Cleaned Task.h file and created Task.cpp and Task.inl

--- src/code/Task.h
+++ src/code/Task.h
@@ -2,7 +2,6 @@
 //
 //	DroneApplication
 //		Author:	Pablo Ramon Soria
-//		Date:	2015-02-07
 //
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -21,49 +20,42 @@
 
 class DroneApplication;	// Forward declaration
 
-/**	Base task class that define asynchronous task interface. Never used directly, used to 
-	*	store tasks multiple types tasks in maps or arrays
-*
-*/
+//---------------------------------------------------------------------------------------------------------------------
+///	Base task class that define asynchronous task interface. Never used directly, used to 
+///	store tasks multiple types tasks in maps or arrays
 class TaskBase: protected Loggeable{
 public:
-	/** \brief Queue new message to drone's controller queue (777 need revision). 
-	*/
-	void	queueMessage	(Message _message)	{ mDroneQueue->push(_message); };
+	/// \brief Queue new message to drone's controller queue (777 need revision). 
+	void	queueMessage	(Message _message);
 
-	/** \brief Get sensor of given template type  777 need revision to generalize in runtime not compile time.
-	*/
+	/// \brief Get sensor of given template type  777 need revision to generalize in runtime not compile time.
 	template<typename SensorClassType_>
-	SensorClassType_* getSensor()	{ return ((SensorClassType_*)(mSensorManager->get(SensorClassType_::Trait::Type))); };
+	SensorClassType_* getSensor();
 
 protected:
 	friend class DroneApplication;
-	TaskBase(): Loggeable("Task") {};
-	void	attach(std::priority_queue<Message, std::vector<Message>, Message> *_queue, SensorManager *_sensorMgr) { mDroneQueue = _queue; mSensorManager = _sensorMgr; };
+	TaskBase();
+	void	attach(std::priority_queue<Message, std::vector<Message>, Message> *_queue, SensorManager *_sensorMgr);
 
 private:
 	std::priority_queue<Message, std::vector<Message>, Message>		*mDroneQueue;
 	SensorManager	*mSensorManager;
 };
 
-/** Task interface that implements the thread of the asyncronous tasks. 777 Add example.
-*
-*/
+//---------------------------------------------------------------------------------------------------------------------
+/// Task interface that implements the thread of the asyncronous tasks. 777 Add example.
 template<typename SonClass_>
 class Task: public TaskBase{
 public:
-	/** \brief Task action. Loop is not implemented, task cycle is defined inside this method.
-	*/
+	/// \brief Task action. Loop is not implemented, task cycle is defined inside this method.
 	virtual void run() = 0;
 
-	/** \brief Start task.
-	*/
-	void start(){
-		mThread = new std::thread(&SonClass_::run, reinterpret_cast<SonClass_*>(this));
-		write2Log("Starting Task");
-	}
+	/// \brief Start task.
+	void start();
 private:
 	std::thread		*mThread;
 };
+
+#include "Task.inl"
 
 #endif	//	TASK_H_
